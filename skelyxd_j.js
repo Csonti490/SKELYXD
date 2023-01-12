f7e86c053e838606d477699656d6bd4d0e406d6c
CsontiXD <113976041+Csonti490@users.noreply.github.com>
2023-01-12 19:47:31 +0100

    Add files via upload

---
+++ skelyxd_j.js
@@ -0,0 +1,153 @@
+/*
+Remnant is the future-fantasy world of RWBY, with airships, gadgets, high-tech weaponry and a form of natural energy called Dust existing side by side.
+RNRJ - https://rwby.fandom.com/wiki/Team_RNJR
+egyik - https://rwby.fandom.com/wiki/Volume_5
+masik - https://tvtropes.org/pmwiki/pmwiki.php/Characters/RWBYWorldOfRemnant
+WIKI - https://en.wikipedia.org/wiki/RWBY
+*/
+var videoSources = {
+    series: [
+        {
+            title: "RWBY 5. évad", 
+            ti: "Sorozat Szinopszis", // https://tvtropes.org/pmwiki/pmwiki.php/Recap/RWBYV5E5NecessarySacrifice - Szinopszis
+            de: `<img src="https://musicart.xboxlive.com/7/84a45000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080" class="kiskep2"><p id ="soriinfo">Team RNJR and Qrow finally arrive in Mistral and are surprised to see Oscar and Ozpin. Weiss' and Yang's plans to rendezvous with the team take a detour while Blake, her family and Sun are dealing with their own fight in Menagerie. Meanwhile, Salem makes her move against Mistral, as she did Vale. Unbeknownst to the heroes, Leonardo Lionheart is in league with her out of fear.<br>During this Volume, Adam Taurus pulls a coup against Sienna Khan and takes over the White Fang while the Maidens are revealed to be closely connected to the Relics.</p>`,
+            episodes: [ // https://www.imdb.com/title/tt3066242/episodes?season=5 - részenként rövid leírás
+                {
+                    title: "Necessary Sacrifice / Szükséges Áldozat",
+                    sourceCode: `<iframe width="660" height="380" frameborder="0" src="https://mega.nz/embed/upEiWTAb#xEbLHhiAdeNQU6d3wbh_FbpoLPWPOUOaQ4KRlMTz6vA" allowfullscreen></iframe>`,
+                    desc: `<p id="lone">1. rész rövid leírása</p><p id="ltwo">Blake and Sun fail to get enough signatures for their petition to save Haven Academy. Ilia is given orders by the Albains to assassinate Blake's parents and capture Blake.</p>`
+                },
+                {
+                    title: "Known by its Song / Felismerni a Daláról",
+                    sourceCode: `<iframe width="660" height="380" frameborder="0" src="https://mega.nz/embed/WhVh2IoR#dXSP9JLHjfBFpBEQ8uxHruO8zm70ijK3yxUdXoLHEKo" allowfullscreen ></iframe>`,
+                    desc: `<p id="lone">2. rész rövid leírása</p><p id="ltwo">Raven explains to Yang and Weiss her distrust in Ozpin after learning about Salem's existence. She shows off her shape shifting ability as proof and offers Yang a choice to stay with her. Yang and Weiss choose to leave and reunite with Ruby.</p>`
+                },
+                {
+                    title: "Rest and Resolutions / Pihenés és Állásfoglalások",
+                    sourceCode: `<iframe width="660" height="380" frameborder="0" src="https://mega.nz/embed/D0FVWJYb#pZH6Aq0nQax-dgnH-pF2KnijCFfifJorQGxdEwtc4A8" allowfullscreen ></iframe> `,
+                    desc: `<p id="lone">3. rész rövid leírása</p><p id="ltwo">Yang confronts Ozpin about using magic on Qrow and Raven, who reveals everything about his powers and the Maidens. Meanwhile, Cinder, Watts, Mercury and Emerald arrive at Raven's camp.</p>`
+                },
+                {
+                    title: "Alone Together / Egyedül Együtt",
+                    sourceCode: `<iframe width="640" height="360" frameborder="0" src="https://mega.nz/embed/uwciRSLQ#j9WYHnDoAj9rHMJEePpvRNJ6VeMH2_Wqj5TGlUmUbak" allowfullscreen ></iframe>`,
+                    desc: `<p id="lone">4. rész rövid leírása</p><p id="ltwo">Emotions heighten as Ruby and Weiss discover that Yang has yet to forgive Blake. Illia's internal conflict grows. Blake discovers that her family is in danger.</p>`
+                }
+            ]
+        },
+        {
+            title: "RWBY: World of Remnant",
+            ti: "Sorozat Szinopszis", // https://www.imdb.com/find/?q=World%20of%20Remnant&ref_=nv_sr_srsg_4 - Szinopszis
+            de: `<img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS1bJ3NYB5IhQgF2tjcndk_t29SV7ToA6Q2HWhyKglsYvtUQhWu" class="kiskep2"><p id ="soriinfo">The World of Remnant is a place full of mythic tales, dangerous monsters, hidden threats and secret histories. While Remnant has recorded the exploits of heroes and sinners, as well as the lives of more ordinary people, clues to an ancient, secret history lie scattered throughout the world's many children's fairy tales.This page is for the characters of history and legend, the fairy tales that hide greater truths, and for living beings that the world may not know even exist outside of myth.</p>`,
+            episodes: [ // https://www.imdb.com/title/tt3066242/episodes?season=-1 - részenkénti rövid leírás
+                {
+                    title: "Dust / Por",
+                    sourceCode: `<iframe width="660" height="380" frameborder="0" src="https://mega.nz/embed/DtMQnS5D#oNb7qSnczIsnF704pwDE3DKjAOqIrm608IFT0WwqHbw" allowfullscreen ></iframe>`,
+                    desc: `<p id="lone">1. rész rövid leírása</p><p id="ltwo">The first episode of a supplementary series looking at the history and lore of Remnant. This episode delves into the mysterious power of Dust Crystals.</p>`
+                },
+                {
+                    title: "Kingdoms / Királyságok",
+                    sourceCode: `- Egyenlőre nincsen elérhető videó -`,
+                    desc: `<p id="lone">2. rész rövid leírása</p><p id="ltwo">The history and lore of Remnant revealing information about the four Kingdom's locations, politics and military structures.</p>`
+                },
+                {
+                    title: "Grimm / Grimm",
+                    sourceCode: `- Egyenlőre nincsen elérhető videó -`,
+                    desc: `<p id="lone">3. rész rövid leírása</p><p id="ltwo">The third episode of a supplementary series looking at the history and lore of Remnant. This episode examines the horrific creatures of Grimm.</p>`
+                }
+            ]
+        },
+        {
+            title: "Openingek",
+            ti: "Szinopszis",
+            de: `<img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRI89wHJogKV2Hx3Zqv34y89PgwaYFAMRuXF9dRp3qTLn1cxsA6" class="kiskep2"><p id ="soriinfo">Kíváncsi voltam, hogy mégis milyen lehet openingeket lefordítani, mivel szerintem teljesen más, mint egy párbeszédet lefordítani.<br><br>Eddigi Openingek: RWBY 5.évad, RWBY 8.évad, SSnVR, TSKD</p>`,
+            episodes: [
+                {
+                    title: "RWBY: The Triumph",
+                    sourceCode: `<iframe width="660" height="380" src="https://www.youtube.com/embed/9HcEHMDQSFM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+                    desc: `<p id="lone">- Leírás -</p><p id="ltwo">Ez a dal a RWBY 5.évad Openingje. "The Triumph"<br>Komponálta: Jeff Williams<br>Énekli: Casey Lee Williams</p>`
+                },
+                {
+                    title: "RWBY: For Every Life",
+                    sourceCode: `<iframe width="660" height="380" src="https://www.youtube.com/embed/yel_We3OW7M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+                    desc: `<p id="lone">- Leírás -</p><p id="ltwo">-Egyenlőre még nincsen leírás-</p>`
+                },
+                {
+                    title: "Shokei Shoujo no Virgin Road: Paper Bouquet",
+                    sourceCode: `<iframe width="660" height="380" src="https://www.youtube.com/embed/iL9NGfxSg7Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+                    desc: `<p id="lone">- Leírás -</p><p id="ltwo">-Egyenlőre még nincsen leírás-</p>`
+                },
+                {
+                    title: "Tensei Shitara Ken Deshita",
+                    sourceCode: `- Egyenlőre még nincsen videó -`,
+                    desc: `<p id="lone">- Leírás -</p><p id="ltwo">-Egyenlőre még nincsen leírás-</p>`
+                }
+            ]
+        }
+    ]
+}
+
+function FirstLoad(){
+    localStorage.setItem('result_was_set', true);
+    if((localStorage.getItem('result_was_set') === "false")) localStorage.setItem('result', 0);
+}
+
+// Melyik sorozat?
+function EpisodeDefault(){
+    /*alert(localStorage.getItem('result'));*/
+    /*if (localStorage.getItem('result') == 1)
+        document.getElementById("pn").innerHTML = "sikeres";
+    else
+        document.getElementById("pn").innerHTML = "sikertelen";*/
+    document.getElementById("pn").innerHTML = `${videoSources.series[(+localStorage.getItem('result')) - 1].title}`;
+    document.getElementById("video").innerHTML = `${videoSources.series[(+localStorage.getItem('result')) - 1].de}`;
+    document.getElementById("episodeTitle").innerHTML = `${videoSources.series[(+localStorage.getItem('result')) - 1].ti}`;
+    /*document.getElementById("pn").innerHTML = `${videoSources.series[(+localStorage.getItem('result')) - 1].description}`;*/
+    let pagination = ``;
+    for(let i = 0; i < videoSources.series[(+localStorage.getItem('result')) - 1].episodes.length; i++){
+        pagination += `
+            <li onmouseover="hover(this)" onmouseout="hoverOff(this)" class="target" onclick="EpisodeChange(${(+i + 1)})">${i + 1}. rész</li>
+        `;
+    }
+    document.getElementById("episodes").innerHTML = pagination;
+
+}
+//document.getElementById("pn").innerHTML
+function ProjectChange(n) {
+    localStorage.removeItem('result');
+    switch (n){
+        case 1:
+            localStorage.setItem('result', 1);
+            break;
+        case 2:
+            localStorage.setItem('result', 2);
+            break;
+        case 3:
+            localStorage.setItem('result', 3);
+            break;
+    }
+}
+
+function EpisodeChange(n) {
+    // Megkeressük a sorozatot a listából
+    let series = (+localStorage.getItem('result')), episode = n;
+    // Lineáris keresés (most epizódot keresünk) sorozat > epizód
+    if(window.location.pathname.includes("/skelyxd_v.html")){
+        console.log("VALID URL")
+        document.getElementById("episodeTitle").textContent = `${videoSources.series[(+localStorage.getItem('result')) - 1].episodes[episode - 1].title}`;
+        document.getElementById("video").innerHTML = `${videoSources.series[(+localStorage.getItem('result')) - 1].episodes[episode - 1].sourceCode}`;
+        document.getElementById("leiras").innerHTML = `${videoSources.series[(+localStorage.getItem('result')) - 1].episodes[episode - 1].desc}`;
+    } else console.log("INVALID URL")
+    // alert(`${videoSources.series[(+localStorage.getItem('result'))].title}`); // <- Kiválasztott sorozat címe
+    // alert(`${videoSources.series[(+localStorage.getItem('result'))].episodes[episode].title}`); // <- Kiválasztott sorozat, kiválasztott epizódjának címe
+    // alert(`${videoSources.series[(+localStorage.getItem('result'))].episodes[episode].sourceCode}`); // <- Kiválasztott sorozat, kiválasztott epizódjának source code-ja.
+}
+
+
+function hover(element)
+{
+    element.style.backgroundColor = "red";
+}
+function hoverOff(element)
+{
+    element.style.backgroundColor = "#1a1a1a";
+}
